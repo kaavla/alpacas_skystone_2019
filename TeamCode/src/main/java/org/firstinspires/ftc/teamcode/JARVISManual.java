07a94c3293bc662431a7a7125f1caef4d59066ce
Shashank <kaavla@gmail.com>
2019-11-23 16:20:09 -0800

    Added right stick code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JARVISManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JARVISManual.java
@@ -26,6 +26,8 @@ public class JARVISManual extends LinearOpMode {
         //This is where we set our motor powers
         double motor_power = 0.7;
 
+        float   leftX, leftY, rightZ;
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         robotJARVIS.init(hardwareMap);
@@ -42,6 +44,11 @@ public class JARVISManual extends LinearOpMode {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
             //all of the code bellow is setting a power to each button on the gamepad
+
+            leftY = gamepad1.left_stick_y;
+            leftX = gamepad1.left_stick_x * -1;
+            rightZ = gamepad1.right_stick_x * -1;
+            robotJARVIS.moveHolonomic(leftX, leftY, rightZ);
 
             if (gamepad1.dpad_up) {
                 //forward
